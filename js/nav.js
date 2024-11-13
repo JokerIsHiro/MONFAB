7030a4a8babafbbd82001695109314514df88b00
Carlos Lozano Martinez <97533586+JokerIsHiro@users.noreply.github.com>
2024-11-13 12:27:22 +0100

    Creados CreateElement2, delete, get y modify

---
+++ js/nav.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('./nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('nav-container').innerHTML = data;
+
+            const currentPage = window.location.pathname.split('/').pop();
+
+            const navItems = document.querySelectorAll(".nav-item");
+
+            navItems.forEach(item => {
+                const linkItem = item.querySelector("a").getAttribute("href");
+                if(linkItem === currentPage){
+                    item.classList.add('active');
+                }
+                item.addEventListener('click', function () {
+                    navItems.forEach(navItem => navItem.classList.remove('active'));
+                    this.classList.add('active');
+                });
+            });
+        });
+});
--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-const data = [{fullname: "Juan",desc: "Hola buenas tardes",serial: "368347863",status: "on",prio: "High"},
-    {fullname: "Juan",desc: "Hola buenas tardes",serial: "368347863",status: "on",prio: "High"},
-    {fullname: "Miguel",desc: "Hola buenas tardes",serial: "368347863",status: "on",prio: "High"},
-    {fullname: "Jose",desc: "Adios buenas tardes",serial: "368347863",status: "off",prio: "Medium"},
-    {fullname: "Victor",desc: "Hola buenas tardes", serial: "59749663856",status: "on",prio: "High"},
-    {fullname: "Alejandro",desc: "Hola buenas tardes",serial: "574697450",status: "off",prio: "Low"}];
+const data = [
+    { fullname: "Juan", desc: "Hola buenas tardes", serial: "368347863", status: "on", prio: "High" },
+    { fullname: "Juan", desc: "Hola buenas tardes", serial: "368347863", status: "on", prio: "High" },
+    { fullname: "Miguel", desc: "Hola buenas tardes", serial: "368347863", status: "on", prio: "High" },
+    { fullname: "Jose", desc: "Adios buenas tardes", serial: "368347863", status: "off", prio: "Medium" },
+    { fullname: "Victor", desc: "Hola buenas tardes", serial: "59749663856", status: "on", prio: "High" },
+    { fullname: "Alejandro", desc: "Hola buenas tardes", serial: "574697450", status: "off", prio: "Low" }
+];
 
 const tbody = document.querySelector('#body');
 const input = document.querySelector('#filter');
+const dialog = document.querySelector('#editDialog')
+const form = document.querySelector('#form');
 
-window.onload = populateTable();
-
+populateTable();
 input.onkeyup = function (event) {
     const val = input.value.toLowerCase();
     if (val.length >= 3) {
@@ -31,19 +34,58 @@ input.onkeyup = function (event) {
 function populateTable() {
     data.forEach(item => {
         const rows = document.createElement('tr');
-        const btn = document.createElement('button');
+        const btnRemove = document.createElement('button');
+        const btnEdit = document.createElement('button');
         const action = document.createElement('td');
-        btn.value = 'borrar';
-        btn.id = 'btn';
-        btn.innerText = 'X';
-        action.appendChild(btn);
+        btnRemove.value = 'borrar';
+        btnRemove.id = 'btnRemove';
+        btnRemove.innerText = 'X';
+        btnEdit.value = 'editar';
+        btnEdit.id = 'btnEdit';
+        btnEdit.innerText = 'Editar';
+        action.appendChild(btnRemove);
+        action.appendChild(btnEdit);
         Object.values(item).forEach(value => {
             const td = document.createElement('td');
-            td.appendChild(document.createTextNode(value));
+            td.textContent = value;
             rows.insertAdjacentElement("afterbegin", action);
             rows.appendChild(td);
         });
-        btn.onclick = () => rows.remove(); 
+        btnRemove.onclick = () => rows.remove();
+        btnEdit.onclick = () => editRowData(item, rows);
         tbody.appendChild(rows);
     });
+}
+
+function editRowData(item, row) {
+
+    document.querySelector('#fullname').placeholder = item.fullname;
+    document.querySelector('#desc').placeholder = item.desc;
+    document.querySelector('#serial').placeholder = item.serial;
+    document.querySelector('#status').checked = item.status === 'on';
+    document.querySelector('#prioHigh').checked = item.prio === 'High';
+    document.querySelector('#prioMedium').checked= item.prio === 'Medium';
+    document.querySelector('#prioLow').checked = item.prio === 'Low';
+
+    dialog.showModal();
+
+    form.onsubmit = (event) => {
+        event.preventDefault(); //Evita que se refresque la pagina al presionar "Save"
+        
+        item.fullname = document.querySelector('input[name="fullname"]').value;
+        item.desc = document.querySelector('input[name="desc"]').value;
+        item.serial = document.querySelector('input[name="serial"]').value;
+        item.status = document.querySelector('input[name="status"]').checked ? "on" : "off";
+        item.prio = document.querySelector('input[name="prio"]:checked').value;
+
+        const cells = row.querySelectorAll('td');
+
+        cells[1].textContent = item.fullname;
+        cells[2].textContent = item.desc;
+        cells[3].textContent = item.serial;
+        cells[4].textContent = item.status;
+        cells[5].textContent = item.prio;
+
+        dialog.close();
+    }
 }
