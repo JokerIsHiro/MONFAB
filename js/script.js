1f52ec44dc42d3352c83a48b923b2fc63b5535ed
Carlos Lozano Martinez <97533586+JokerIsHiro@users.noreply.github.com>
2024-11-18 16:24:16 +0100

    Cambios en JS

--- js/script.js
+++ js/script.js
@@ -64,27 +64,48 @@ function editRowData(item, row) {
     document.querySelector('#serial').placeholder = item.serial;
     document.querySelector('#status').checked = item.status === 'on';
     document.querySelector('#prioHigh').checked = item.prio === 'High';
-    document.querySelector('#prioMedium').checked= item.prio === 'Medium';
+    document.querySelector('#prioMedium').checked = item.prio === 'Medium';
     document.querySelector('#prioLow').checked = item.prio === 'Low';
 
     dialog.showModal();
 
+
     form.onsubmit = (event) => {
         event.preventDefault(); //Evita que se refresque la pagina al presionar "Save"
-        
-        item.fullname = document.querySelector('input[name="fullname"]').value;
-        item.desc = document.querySelector('input[name="desc"]').value;
-        item.serial = document.querySelector('input[name="serial"]').value;
-        item.status = document.querySelector('input[name="status"]').checked ? "on" : "off";
-        item.prio = document.querySelector('input[name="prio"]:checked').value;
+
+        const newName = document.querySelector('input[name="fullname"]').value;
+        const newDesc = document.querySelector('input[name="desc"]').value;
+        const newSerial = document.querySelector('input[name="serial"]').value;
+        const newStatus = document.querySelector('input[name="status"]').checked ? "on" : "off";
+        const newPrio = document.querySelector('input[name="prio"]:checked').value;
 
         const cells = row.querySelectorAll('td');
 
-        cells[1].textContent = item.fullname;
-        cells[2].textContent = item.desc;
-        cells[3].textContent = item.serial;
-        cells[4].textContent = item.status;
-        cells[5].textContent = item.prio;
+        if (newName && newName !== item.fullname) {
+            item.fullname = newName;
+            cells[1].textContent = item.fullname;
+            cells[1].style.color = 'yellow';
+        }
+        if (newDesc && newDesc !== item.desc) {
+            item.desc = newDesc;
+            cells[2].textContent = item.desc;
+            cells[2].style.color = 'yellow';
+        }
+        if (newSerial && newSerial !== item.serial) {
+            item.serial = newSerial;
+            cells[3].textContent = item.serial;
+            cells[3].style.color = 'yellow';
+        }
+        if (newStatus && newStatus !== item.status) {
+            item.status = newStatus;
+            cells[4].textContent = item.status;
+            cells[4].style.color = 'yellow';
+        }
+        if (newPrio && newPrio !== item.prio) {
+            item.prio = newPrio;
+            cells[5].textContent = item.prio;
+            cells[5].style.color = 'yellow';
+        }
 
         dialog.close();
     }
